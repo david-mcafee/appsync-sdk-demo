62866a1df776512e04f06d2dad49116477abaefa
mcafd@amazon.com <mcafd@amazon.com>
2021-08-04 16:57:01 -0700

    basic working demo

---
+++ src/WithProvider.js
@@ -0,0 +1,26 @@
+import AWSAppSyncClient from "aws-appsync";
+import AppSyncConfig from "./aws-exports";
+import { ApolloProvider } from "react-apollo";
+import { Rehydrated } from "aws-appsync-react"; // this needs to also be installed when working with React
+
+import App from "./App";
+
+const client = new AWSAppSyncClient({
+  url: AppSyncConfig.aws_appsync_graphqlEndpoint,
+  region: AppSyncConfig.aws_appsync_region,
+  auth: {
+    type: AppSyncConfig.aws_appsync_authenticationType,
+    apiKey: AppSyncConfig.aws_appsync_apiKey,
+    // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+  },
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
+export default WithProvider;
