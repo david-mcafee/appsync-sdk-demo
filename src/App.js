6b81da1f88bfc04a6787966391a7774cba2fbed3
mcafd@amazon.com <mcafd@amazon.com>
2021-08-06 13:15:47 -0700

    apollo V3 demo

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render } from "react-dom";
+import { gql } from "@apollo/client";
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      rates(currency: "USD") {
+        currency
+        rate
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
+    </div>
+  );
+}
+
+render(<App />);
---
+++ src/WithProvider.js
@@ -0,0 +1,28 @@
+// import AppSyncConfig from "./aws-exports";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+import App from "./App";
+
+// const client = new AWSAppSyncClient({
+//   url: AppSyncConfig.aws_appsync_graphqlEndpoint,
+//   region: AppSyncConfig.aws_appsync_region,
+//   auth: {
+//     type: AppSyncConfig.aws_appsync_authenticationType,
+//     apiKey: AppSyncConfig.aws_appsync_apiKey,
+//     // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+//   },
+// });
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
+
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+export default WithProvider;
