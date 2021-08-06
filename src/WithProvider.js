1b6c6caacb72bc4baea2b0a143790e44c87e3855
mcafd@amazon.com <mcafd@amazon.com>
2021-08-06 14:15:21 -0700

    api test

--- src/WithProvider.js
+++ src/WithProvider.js
@@ -1,4 +1,4 @@
-// import AppSyncConfig from "./aws-exports";
+import AppSyncConfig from "./aws-exports";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 import App from "./App";
@@ -14,9 +14,20 @@ import App from "./App";
 // });
 
 const client = new ApolloClient({
-  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  uri: AppSyncConfig.aws_appsync_graphqlEndpoint,
+  region: AppSyncConfig.aws_appsync_region,
+  auth: {
+    type: AppSyncConfig.aws_appsync_authenticationType,
+    apiKey: AppSyncConfig.aws_appsync_apiKey,
+    // jwtToken: async () => token, // Required when you use Cognito UserPools OR OpenID Connect. token object is obtained previously
+  },
   cache: new InMemoryCache(),
 });
+
+// const client = new ApolloClient({
+//   uri: "https://48p1r2roz4.sse.codesandbox.io",
+//   cache: new InMemoryCache(),
+// });
 
 const WithProvider = () => (
   <ApolloProvider client={client}>
